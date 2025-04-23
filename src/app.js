df2be9a35f0e8daa7ec3476c946593cc4eca234c
Rafaelbarrozzi <rafaeljosebarrozzi@gmail.com>
2025-04-23 19:12:07 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // function combinacion () {
+  //   const pronoun = ["the", "our"]
+  //   const adj = ["great", "big"]
+  //   const noun = ["jogger", "racoon"]
+
+  //   combinacion.forEach((posicion, indice ) => {
+  //     console.log(combinacion)
+  //   })
+    
+  //   };
+  
+}
+function combinacion () {
+  const pronoun = ["the", "our"]
+  const adj = ["great", "big"]
+  const noun = ["jogger", "racoon"]
+
+let combinador = []
+for(let i = 0; i<pronoun.length; i++){
+  for(let j = 0; j<adj.length; j++){
+    for(let k = 0; k<noun.length; k++) {
+      combinador.push(pronoun[i] + adj[j] + noun[k]+".com")
+    }
+    }
+}
+return combinador
+}
+console.log(combinacion());
+
